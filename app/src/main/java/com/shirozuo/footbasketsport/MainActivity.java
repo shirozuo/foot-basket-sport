c2e48553a8d9c1eaf096f20905a0b4da9dc9cee8
shirozuo <shiroyonago@gmail.com>
2021-06-07 22:18:37 +0300

    Interim update for save code Added new files: menu_bottom_navigation.xml & bottom_navigation_item_background_colors.xml
    Filled MainActivity.java class some methods for add new functional
    
    Added RecyclerView & BottomNavigationBar in activity_main.xml

--- app/src/main/java/com/shirozuo/footbasketsport/MainActivity.java
+++ app/src/main/java/com/shirozuo/footbasketsport/MainActivity.java
@@ -1,13 +1,58 @@
 package com.shirozuo.footbasketsport;
 
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgPeoples, imgGallery;
+    private RecyclerView rules, peoples, gallery;
+    private RulesAdapter rulesAdapter;
+    private String typeOfRules;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rules = findViewById(R.id.rv_rules);
+        imgPeoples = findViewById(R.id.icon_peoples);
+        imgGallery = findViewById(R.id.icon_gallery);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rules.setLayoutManager(linearLayoutManager);
+
+
+
+
+        rulesAdapter = new RulesAdapter(12, R.array.basketball_rules);
+        rules.setAdapter(rulesAdapter);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_rules:
+                    rules.setVisibility(View.VISIBLE);
+                    imgPeoples.setVisibility(View.GONE);
+                    imgGallery.setVisibility(View.GONE);
+                    break;
+                case R.id.action_peoples:
+                    rules.setVisibility(View.GONE);
+                    imgPeoples.setVisibility(View.VISIBLE);
+                    imgGallery.setVisibility(View.GONE);
+                    break;
+                case R.id.action_gallery:
+                    rules.setVisibility(View.GONE);
+                    imgPeoples.setVisibility(View.GONE);
+                    imgGallery.setVisibility(View.VISIBLE);
+            }
+            return true;
+        });
     }
 }
