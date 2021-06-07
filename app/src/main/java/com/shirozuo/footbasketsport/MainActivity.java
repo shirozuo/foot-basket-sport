97ec93de2b3c6fba147f29d5b9bf7c94c28fe4d5
shirozuo <shiroyonago@gmail.com>
2021-06-07 13:56:43 +0300

    Updated for new content

--- app/src/main/java/com/shirozuo/footbasketsport/MainActivity.java
+++ app/src/main/java/com/shirozuo/footbasketsport/MainActivity.java
@@ -1,5 +1,7 @@
 package com.shirozuo.footbasketsport;
 
+import android.content.Intent;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splashscreen);
+        new Handler().postDelayed((Runnable) () -> {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3*1000);
     }
 }
