5a81d36fdc9d5bc3d64d5edecf59b9b9351489bd
shirozuo <shiroyonago@gmail.com>
2021-06-08 02:58:45 +0300

    Updated by example of previous adapters

--- app/src/main/java/com/shirozuo/footbasketsport/MainActivity.java
+++ app/src/main/java/com/shirozuo/footbasketsport/MainActivity.java
@@ -1,8 +1,6 @@
 package com.shirozuo.footbasketsport;
 
-import android.content.res.TypedArray;
 import android.view.View;
-import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,11 +9,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imgGallery;
     private RecyclerView rules, players, gallery;
-    private RulesAdapter rulesAdapter;
-    private PlayersAdapter playersAdapter;
-    private String typeOfRules;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
         rules = findViewById(R.id.rv_rules);
         players = findViewById(R.id.rv_players);
-        imgGallery = findViewById(R.id.icon_gallery);
+        gallery = findViewById(R.id.rv_gallery);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(this);
+        LinearLayoutManager linearLayoutManager3 = new LinearLayoutManager(this);
+
         rules.setLayoutManager(linearLayoutManager);
         players.setLayoutManager(linearLayoutManager2);
+        gallery.setLayoutManager(linearLayoutManager3);
 
-        playersAdapter = new PlayersAdapter(10, R.array.basketball_players, R.array.basketball_players_image);
+        players.setHasFixedSize(true);
+        PlayersAdapter playersAdapter = new PlayersAdapter(10, R.array.basketball_players, R.array.basketball_players_image);
         players.setAdapter(playersAdapter);
-        rulesAdapter = new RulesAdapter(12, R.array.basketball_rules);
+
+        rules.setHasFixedSize(true);
+        RulesAdapter rulesAdapter = new RulesAdapter(12, R.array.basketball_rules);
         rules.setAdapter(rulesAdapter);
+
+        gallery.setHasFixedSize(true);
+        GalleryAdapter galleryAdapter = new GalleryAdapter(10, R.array.basketball_image);
+        gallery.setAdapter(galleryAdapter);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
@@ -43,17 +47,17 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.action_rules:
                     rules.setVisibility(View.VISIBLE);
                     players.setVisibility(View.GONE);
-                    imgGallery.setVisibility(View.GONE);
+                    gallery.setVisibility(View.GONE);
                     break;
                 case R.id.action_peoples:
                     rules.setVisibility(View.GONE);
                     players.setVisibility(View.VISIBLE);
-                    imgGallery.setVisibility(View.GONE);
+                    gallery.setVisibility(View.GONE);
                     break;
                 case R.id.action_gallery:
                     rules.setVisibility(View.GONE);
                     players.setVisibility(View.GONE);
-                    imgGallery.setVisibility(View.VISIBLE);
+                    gallery.setVisibility(View.VISIBLE);
             }
             return true;
         });
