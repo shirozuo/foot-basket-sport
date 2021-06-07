d10e9df7df4234f2c371cd03937f72a44948f5aa
shirozuo <shiroyonago@gmail.com>
2021-06-07 14:28:37 +0300

    Updated for new content Added buttons clickable

--- app/src/main/java/com/shirozuo/footbasketsport/HomeActivity.java
+++ app/src/main/java/com/shirozuo/footbasketsport/HomeActivity.java
@@ -1,13 +1,30 @@
 package com.shirozuo.footbasketsport;
 
+import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button buttonFootball, buttonBasketball;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        buttonFootball = findViewById(R.id.home_football_button);
+        buttonBasketball = findViewById(R.id.home_basketball_button);
+        click();
+    }
+    private void click(){
+        buttonFootball.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+        buttonBasketball.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
