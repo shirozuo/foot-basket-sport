f1eeb2da1dc6fbf731420e5884e686e484578f01
shirozuo <shiroyonago@gmail.com>
2021-06-08 02:05:15 +0300

    Added content for second tab of bottomNavigationBar

---
+++ app/src/main/java/com/shirozuo/footbasketsport/PlayersAdapter.java
@@ -0,0 +1,71 @@
+package com.shirozuo.footbasketsport;
+
+import android.content.res.TypedArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PlayersAdapter extends RecyclerView.Adapter<PlayersAdapter.PlayerViewHolder> {
+
+    private final int playerItems;
+    private List<String> playersName = new ArrayList<>();
+    private TypedArray playersImage;
+    private final int typeOfPlayersName;
+    private final int typeOfPlayersImage;
+
+
+    public PlayersAdapter(int playerItems, int typeOfPlayersName, int typeOfPlayersImage) {
+        this.playerItems = playerItems;
+        this.typeOfPlayersName = typeOfPlayersName;
+        this.typeOfPlayersImage = typeOfPlayersImage;
+    }
+
+    @NonNull
+    @Override
+    public PlayerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.rv_players, parent, false);
+
+        playersName = Arrays.asList(parent.getContext().getResources().getStringArray(typeOfPlayersName));
+        playersImage = parent.getContext().getResources().obtainTypedArray(typeOfPlayersImage);
+
+        return new PlayerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlayerViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return playerItems;
+    }
+
+    class PlayerViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivPlayerImage;
+        TextView tvPlayerName;
+
+
+        public PlayerViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ivPlayerImage = itemView.findViewById(R.id.iv_player_item);
+            tvPlayerName = itemView.findViewById(R.id.tv_view_holder_player);
+        }
+        void bind(int position){
+            ivPlayerImage.setImageResource(playersImage.getResourceId(position, -1));
+            tvPlayerName.setText(playersName.get(position));
+        }
+    }
+}
