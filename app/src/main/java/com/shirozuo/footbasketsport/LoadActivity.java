b4ccc94b6d45345d088aa36f937047468f6424a2
shirozuo <shiroyonago@gmail.com>
2021-06-08 12:01:12 +0300

    Updated to prevent errors on Android API lower 30

--- app/src/main/java/com/shirozuo/footbasketsport/LoadActivity.java
+++ app/src/main/java/com/shirozuo/footbasketsport/LoadActivity.java
@@ -1,11 +1,13 @@
 package com.shirozuo.footbasketsport;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoadActivity extends AppCompatActivity {
+public class LoadActivity extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 3000; //3000ms
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +15,8 @@ public class LoadActivity extends AppCompatActivity {
         setContentView(R.layout.splashscreen);
         new Handler().postDelayed((Runnable) () -> {
             Intent intent = new Intent(LoadActivity.this, HomeActivity.class);
-            startActivity(intent);
-            finish();
-        }, 3*1000);
+            LoadActivity.this.startActivity(intent);
+            LoadActivity.this.finish();
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
